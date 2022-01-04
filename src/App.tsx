63e76427ba45c4fb71d1dcd9bc9e3f410691a5d4
Hasnaa Ayman <hassnaaayman16@gmail.com>
2022-01-04 20:14:18 +0200

    feat(default-screen): finish the default screen ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+import githubLogo from './assets/svg/iconmonstr-github.svg';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <img src={githubLogo} className='App-logo' alt='logo' />
+      <Input
+        type='search'
+        placeholder='search for Github repositories'
+        name='q'
+      />
+    </Container>
   );
-}
+};
 
 export default App;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  width: 90%;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  padding-left: 41px;
+  background: url('https://img.icons8.com/ios-glyphs/30/000000/search--v1.png')
+    no-repeat left;
+  background-size: 20px;
+  width: 23%;
+  height: 40px;
+  border: 1px solid #cccc;
+  background-position-x: 5px;
+  margin-top: 30px;
+  text-align: start;
+`;
